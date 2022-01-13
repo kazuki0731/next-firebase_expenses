257457f2e94450c54535b28c4d54213c0e8a34a9
kawabata <kazuki.kawabata02@gmail.com>
2022-01-13 20:26:54 +0900

    top完成1/13

--- components/top/introduction.tsx
+++ components/top/introduction.tsx
@@ -1,39 +1,52 @@
 import { Box, Button, Text } from "@chakra-ui/react";
 import React from "react";
 import PageLink from "../common/pageLink";
+import Logo from "./logo";
 
 const Introduction = () => {
   return (
     <Box
       w={{ base: "300px", md: "600px" }}
       position="absolute"
-      top="50%"
+      top={{ base: "45%", sm: "40%" }}
       left="50%"
       transform="translate(-50%, -50%)"
     >
+      <Text
+        textAlign="center"
+        fontWeight="bold"
+        color="blue"
+        fontFamily="serif"
+        fontSize={{ base: "16px", sm: "24px", md: "38px" }}
+      >
+        カンタン入力で
+        <br />
+        便利な家計簿アプリ
+      </Text>
+      <Text
+        mb="10px"
+        textAlign="center"
+        fontWeight="bold"
+        color="blue.500"
+        fontFamily="serif"
+        fontSize={{ base: "10px", sm: "14px", md: "24px" }}
+      >
+        目標を立てて楽しく記帳
+      </Text>
       <PageLink href="/signup">
         <Button
           borderRadius="3px"
           color="#fff"
-          fontSize={{ base: "14px", md: "22px" }}
-          w={{ base: "90px", md: "170px" }}
-          h={{ base: "50px", md: "70px" }}
+          fontSize={{ base: "12px", sm: "16px", md: "22px" }}
+          w={{ base: "70px", sm: "110px", md: "170px" }}
+          h={{ base: "45px", sm: "50px", md: "70px" }}
           m="10px auto"
-          bg="linear-gradient(blue, pink)"
+          bg="linear-gradient(blue, lightblue)"
           _hover={{ opacity: 0.85 }}
         >
           新規登録
         </Button>
       </PageLink>
-      <Text
-        textAlign="left"
-        fontWeight="bold"
-        color="purple"
-        fontFamily="serif"
-        fontSize={{ base: "20px", md: "34px" }}
-      >
-        ここにアプリの説明文が入りますここにアプリの説明文が入りますここにアプリの説明文が入ります
-      </Text>
     </Box>
   );
 };
--- components/top/logo.tsx
+++ components/top/logo.tsx
@@ -6,8 +6,8 @@ const Logo = () => {
     <Box position="absolute" top="18px" left={{ base: "10px", md: "30px" }}>
       <Image
         src="/images/logo.png"
-        width={{ base: "130px", md: "260px" }}
-        height={{ base: "23px", md: "32px" }}
+        width={{ base: "140px", md: "300px" }}
+        height={{ base: "23px", md: "42px" }}
         alt="logo"
       />
     </Box>
